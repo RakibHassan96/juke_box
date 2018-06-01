bd231466e377ae4d9947a0594b3fa5b533c8e6b5
skabir8 <shariar.k8@gmail.com>
2018-06-01 11:12:44 -0400

    added changes

--- actions.js
+++ actions.js
@@ -11,17 +11,32 @@ function song(name, source, image) {
 
 jukebox.prototype.addSong = function(name, source, image){
   var new_song = new song(name, source, image);
-  this.jukebox.push(new_song);
+  this.songs.push(new_song);
 };
 
 
 jukebox.prototype.getSong = function(index){
-  var my_song = this.jukebox[index];
+  var my_song = this.songs[index];
   return my_song;
 };
 
 jukebox.prototype.list = function(){
-  for (i = 0; i < this.jukebox.length ; i++){
-    console.log("Name is: " + this.jukebox[i].name);
+  for (i = 0; i < this.songs.length ; i++){
+    console.log("Name is: " + this.songs[i].name);
   }
 };
+
+var my_juke = new jukebox();
+my_juke.addSong("name","k2","img");
+my_juke.addSong("name2","k22","img2");
+my_juke.addSong("name3","k23","img3");
+my_juke.addSong("name4","k24","img4");
+my_juke.addSong("name5","k25","img5");
+
+
+
+console.log(my_juke.getSong(0).name);
+console.log(my_juke.getSong(0).source);
+console.log(my_juke.getSong(0).image);
+
+my_juke.list();
